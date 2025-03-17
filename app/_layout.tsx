b166ca12f28290ce32cd6f687d1e203a8246a5c9
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2025-03-16 21:26:05 -0300

    [feat]: navtabs on application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
-import React from 'react';
-
-import './global.css'
+import React from "react";
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
---
+++ app/home/index.tsx
@@ -0,0 +1,19 @@
+// HOME SCREEN
+import { Text, TouchableOpacity, View } from "react-native";
+import { useRouter } from "expo-router";
+
+
+export default function Home() {
+    const router = useRouter();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home without anything in it for now</Text>
+    </View>
+  );
+}
