c51620ab46b18343b2756d19ca529075d41f4fd9
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2025-03-24 09:01:53 -0300

    [feat]: Add support for multiple expense entries

--- app/home/refund_request.tsx
+++ app/home/refund_request.tsx
@@ -6,9 +6,58 @@ import { formatCurrency } from '@/utils/formmatters';
 import * as ImagePicker from 'expo-image-picker';
 import Refund from '@/services/routes/refund';
 
+import { Avatar, Icon, ListItem } from '@rneui/themed';
+
 const refund = new Refund()
 
 const RefundRequestScreen = () => {
+
+    const [accordions, setAccordions] = useState([
+        { 
+            id: 1, refundType: '', 
+            description: '', 
+            receiptUri: null as string | null, 
+            totalValue: 0, isOffLimit: false 
+        }
+    ]);
+
+    const [expandedAccordionId, setExpandedAccordionId] = useState<number | null>(null);
+
+    const addAccordion = () => {
+        setAccordions([...accordions,
+            {
+                id: accordions.length + 1,
+                refundType: '',
+                description: '',
+                receiptUri: null,
+                totalValue: 0,
+                isOffLimit: false,
+            }
+        ]);
+    };
+
+    const deleteAccordion = (id: number) => {
+        const updatedAccordions = accordions.filter((accordion) => accordion.id !== id)
+            .map((accordion, index) => ({ ...accordion, id: index + 1 }));
+        setAccordions(updatedAccordions);
+
+        if (expandedAccordionId === id) {
+            setExpandedAccordionId(null);
+        }
+    };
+
+    const toggleAccordion = (id: number) => {
+        setExpandedAccordionId(expandedAccordionId === id ? null : id);
+    };
+
+    const updateAccordion = (id: number, field: string, value: any) => {
+        setAccordions((prevAccordions) =>
+            prevAccordions.map((accordion) =>
+                accordion.id === id ? { ...accordion, [field]: value } : accordion
+            )
+        );
+    };
+
     const [refundType, setRefundType] = useState('');
     const [description, setDescription] = useState('');
     const [amount, setAmount] = useState('');
@@ -22,7 +71,7 @@ const RefundRequestScreen = () => {
     const limit = 1000
 
 
-    const handleImageUpload = async () => {
+    const handleImageUpload = async (id: number) => {
         // Requests permission
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (status !== 'granted') {
@@ -38,7 +87,7 @@ const RefundRequestScreen = () => {
         });
 
         if (!result.canceled) {
-            setReceiptUri(result.assets[0].uri); // Stores the image URI
+            updateAccordion(id, 'receiptUri', result.assets[0].uri); // Updates the specific accordion's receiptUri
         }
     };
 
@@ -46,6 +95,7 @@ const RefundRequestScreen = () => {
         setReceiptUri(null)
     }
 
+    //fix total value
     useEffect(() => {
         if (amount) {
             const quantity = parseFloat(amount);
@@ -68,6 +118,7 @@ const RefundRequestScreen = () => {
         setIsOffLimit(totalValue > limit)
     })
 
+    //Fix handle submit
     const handleSubmit = async () => {
         if(refundType && amount && receiptUri) {
             if(isOffLimit) {
@@ -96,91 +147,135 @@ const RefundRequestScreen = () => {
 
     return (
         <ScrollView className="p-5 bg-gray-50 h-full">
+
             <Text className="text-2xl font-bold text-center mb-6">Pedido de Reembolso</Text>
 
-            <Text className="mb-2 text-lg font-bold">Tipo de Reembolso</Text>
-            <DropDownPicker
-                open={dropdownOpen}
-                value={refundType}
-                items={[
-                    {
-                        label: 'Valor',
-                        value: 'value',
-                        icon: () => <Ionicons name="pricetag" size={20} color="gray" style={{ marginRight: 10 }} />,
-                    },
-                    {
-                        label: 'Quantidade',
-                        value: 'quantity',
-                        icon: () => <Ionicons name="car-sport" size={20} color="gray" style={{ marginRight: 10 }} />,
-                    },
-                ]}
-                setOpen={setDropdownOpen}
-                setValue={setRefundType}
-                placeholder="Escolha o Tipo de Reembolso"
-                style={{ borderColor: '#ccc' }}
-                containerStyle={{ marginBottom: 20 }}
-                placeholderStyle={{
-                    fontSize: 16,
-                    fontWeight: '400',
-                    marginLeft: 10,
-                }}
-                />
+            {accordions.map((accordion) => (
+                <ListItem.Accordion
+                    key={accordion.id}
+                    content={
+                        <>
+                            <Ionicons name={"cash"} size={30} color={"gray"} />
+                            <ListItem.Content>
+                                <ListItem.Title>Despesa {accordion.id}</ListItem.Title>
+                            </ListItem.Content>
+                        </>
+                    }
+                    isExpanded={expandedAccordionId === accordion.id}
+                    onPress={() => toggleAccordion(accordion.id)}
+                >
 
-            <Text className="mb-2 text-lg font-bold">Descrição</Text>
-            <View className="flex-row items-center bg-white p-2 rounded-lg border border-[#ccc] mb-4">
-                <Ionicons name="pencil" size={20} color="#6B7280" />
-                <TextInput
-                    value={description}
-                    onChangeText={setDescription}
-                    placeholder="Descreva a razão do reembolso"
-                    multiline
-                    className="ml-2 w-full h-24 text-left"
+                    {/* Conteúdo do Accordion */}
+                    
+
+                    {/*not working...?*/}
+                    <Text className="mb-2 text-lg font-bold">Tipo de Despesa</Text>
+                    <DropDownPicker
+                        open={dropdownOpen}
+                        value={accordion.refundType}
+                        items={[
+                            {
+                                label: 'Valor',
+                                value: 'value',
+                                icon: () => <Ionicons name="pricetag" size={20} color="gray" style={{ marginRight: 10 }} />,
+                            },
+                            {
+                                label: 'Quantidade',
+                                value: 'quantity',
+                                icon: () => <Ionicons name="car-sport" size={20} color="gray" style={{ marginRight: 10 }} />,
+                            },
+                        ]}
+                        setOpen={setDropdownOpen}
+                        setValue={(value) => updateAccordion(accordion.id, 'refundType', value)}
+                        placeholder="Escolha o Tipo de Despesa"
+                        style={{ borderColor: '#ccc' }}
+                        containerStyle={{ marginBottom: 20 }}
+                        placeholderStyle={{
+                            fontSize: 16,
+                            fontWeight: '400',
+                            marginLeft: 10,
+                        }}
                     />
-            </View>
 
-            <Text className="mb-2 text-lg font-bold">Valor</Text>
-                {isOffLimit ? <Text className="text-lg pb-2 font-bold text-[#ff4a11]"> Você está acima do limite de {limit} </Text> : <></>}
-            <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff', padding: 10, borderRadius: 8, borderColor: '#ccc', marginBottom: 20 }}>
-                <MaterialIcons name={refundType == 'value' ?  'attach-money' : 'add-box'} size={20} color="#6B7280" />
-                <TextInput
-                    value={amount}
-                    onChangeText={setAmount}
-                    placeholder="Insira o Valor do Reembolso"
-                    keyboardType="numeric"
-                    style={{ marginLeft: 10, flex: 1 }}
-                />
-                {refundType === 'quantity' && (
-                    <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
-                        <Text style={{ color: '#6B7280', fontSize: 14 }}>
-                            <FontAwesome name="times" size={16} color="#6B7280" /> {quantityMult}
-                        </Text>
+                    <Text className="mb-2 text-lg font-bold">Descrição</Text>
+                    <View className="flex-row items-center bg-white p-2 rounded-lg border border-[#ccc] mb-4">
+                        <Ionicons name="pencil" size={20} color="#6B7280" />
+                        <TextInput
+                            value={accordion.description}
+                            onChangeText={(text) => updateAccordion(accordion.id, 'description', text)}
+                            placeholder="Descreva a razão da despesa"
+                            multiline
+                            className="ml-2 w-full h-24 text-left"
+                        />
                     </View>
-                )}
-            </View>
+
+                    <Text className="mb-2 text-lg font-bold">Valor</Text>
+                        {isOffLimit ? <Text className="text-lg pb-2 font-bold text-[#ff4a11]"> Você está acima do limite de {limit} </Text> : <></>}
+                    <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff', padding: 10, borderRadius: 8, borderColor: '#ccc', marginBottom: 20 }}>
+                        <MaterialIcons name={accordion.refundType == 'value' ?  'attach-money' : 'add-box'} size={20} color="#6B7280" />
+                        <TextInput
+                            value={accordion.totalValue.toString()}
+                            onChangeText={(text) => updateAccordion(accordion.id, 'totalValue', text)}
+                            placeholder="Insira o Valor da Despesa"
+                            keyboardType="numeric"
+                            style={{ marginLeft: 10, flex: 1 }}
+                        />
+                        {accordion.refundType === 'quantity' && (
+                            <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
+                                <Text style={{ color: '#6B7280', fontSize: 14 }}>
+                                    <FontAwesome name="times" size={16} color="#6B7280" /> {quantityMult}
+                                </Text>
+                            </View>
+                        )}
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={() => handleImageUpload(accordion.id)}
+                        className="bg-blue-500 p-3 rounded-lg flex-row items-center mb-6"
+                    >
+                        <Ionicons name="image-outline" size={20} color="white" />
+                        <Text className="text-white ml-2">Adicionar Recibo</Text>
+                    </TouchableOpacity>
+
+                    {accordion.receiptUri && (
+                        <View className="mb-6">
+                            <Text className="text-lg mb-2">Imagem recebida</Text>
+                            <MaterialIcons name={refundType == 'value' ?  'close' : 'close'} size={20} color="#FF0000" onPress={removeImage}/>
+                            <Image
+                                source={{ uri: accordion.receiptUri || undefined }}
+                                style={{ width: 150, height: 150, borderRadius: 8 }}
+                            />
+                        </View>
+                    )}
+
+                        <View className="bg-[#f0f0f0] p-4 rounded-lg mb-6">
+                            <Text className="text-sm font-bold text-[#6B7280] pb-2">VALOR TOTAL</Text>
+                            <Text className="text-4xl font-bold ">{formatCurrency(accordion.totalValue)}</Text>
+                        </View>
+
+
+
+
+                    {/* Botão para deletar o Accordion */}
+                    <TouchableOpacity
+                        className="bg-red-500 mb-10 p-2 rounded-lg mt-4"
+                        onPress={() => deleteAccordion(accordion.id)} // Botão para deletar o Accordion
+                    >
+                        <Text className="text-white text-center font-bold">Deletar Despesa</Text>
+                    </TouchableOpacity>
+                
+                </ListItem.Accordion>
+            ))}
+
+                
+
 
             <TouchableOpacity
-                onPress={handleImageUpload}
-                className="bg-blue-500 p-3 rounded-lg flex-row items-center mb-6"
+                className="w-full p-3 mb-10 rounded-lg bg-blue-500"
+                onPress={addAccordion} // Botão para adicionar um novo Accordion
             >
-                <Ionicons name="image-outline" size={20} color="white" />
-                <Text className="text-white ml-2">Adicionar Recibo</Text>
+                <Text className="text-white text-center font-bold">Adicionar Despesa</Text>
             </TouchableOpacity>
-
-            {receiptUri && (
-                <View className="mb-6">
-                    <Text className="text-lg mb-2">Imagem recebida</Text>
-                    <MaterialIcons name={refundType == 'value' ?  'close' : 'close'} size={20} color="#FF0000" onPress={removeImage}/>
-                    <Image
-                        source={{ uri: receiptUri }}
-                        style={{ width: 150, height: 150, borderRadius: 8 }}
-                    />
-                </View>
-            )}
-
-                <View className="bg-[#f0f0f0] p-4 rounded-lg mb-6">
-                    <Text className="text-sm font-bold text-[#6B7280] pb-2">VALOR TOTAL</Text>
-                    <Text className="text-4xl font-bold ">{formatCurrency(totalValue)}</Text>
-                </View>
 
             <TouchableOpacity
                 className="w-full p-3 mb-10 rounded-lg bg-green-500"
@@ -188,6 +283,7 @@ const RefundRequestScreen = () => {
             >
                 <Text className="text-white text-center font-bold">Enviar Pedido de Reembolso</Text>
             </TouchableOpacity>
+
         </ScrollView>
     );
 };
