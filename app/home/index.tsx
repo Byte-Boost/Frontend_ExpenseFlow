eac09a631c727c183290cee2bb31d2b7adc462d2
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2025-05-23 14:59:20 -0300

    [feat]: adding statistics on home

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,19 +1,34 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import RefundService from "@/services/refundService";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import ProjectService from "@/services/projectService";
 import { formatCurrency } from "@/utils/formmatters";
-import { useFocusEffect } from "@react-navigation/native"; // importando useFocusEffect
-import { AntDesign, FontAwesome5, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import { useFocusEffect } from "@react-navigation/native";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 const _refundService = new RefundService();
 const _projectService = new ProjectService();
 
 export default function Home() {
   const router = useRouter();
+  const currentDate = new Date();
+
   const [lastRefundData, setLastRefundData] = useState<RefundItem | null>(null);
+  const [displayMonth, setDisplayMonth] = useState(currentDate.getMonth() + 1);
   const [items, setItems] = useState<{ label: string; value: string }[]>([]);
+  const [displayYear, setDisplayYear] = useState(currentDate.getFullYear());
+  const [selectedTotal, setSelectedTotal] = useState<string | null>(null);
+  const [refunds, setRefunds] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (displayMonth < 1) {
+    setDisplayMonth(12);
+    setDisplayYear(displayYear - 1);
+  } else if (displayMonth > 12) {
+    setDisplayMonth(1);
+    setDisplayYear(displayYear + 1);
+  }
 
   interface RefundItem {
     id: string;
@@ -40,6 +55,21 @@ export default function Home() {
     rejected: "border-l-red-400",
     approved: "border-l-green-400",
   };
+
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
 
   const renderLastRefund = ({ item }: { item: RefundItem }) => (
     <TouchableOpacity className="ml-4">
@@ -85,7 +115,6 @@ export default function Home() {
     }
   };
 
-  // useFocusEffect para carregar projetos ao focar na tela
   useFocusEffect(
     useCallback(() => {
       const fetchProjects = async () => {
@@ -106,7 +135,27 @@ export default function Home() {
     }, [])
   );
 
-  // useFocusEffect para carregar último reembolso ao focar na tela
+  useFocusEffect(
+      useCallback(() => {
+        const fetchRefunds = async () => {
+          try {
+            const response = await _refundService.getRefunds(
+              displayMonth.toString(),
+              displayYear.toString()
+            );
+            setRefunds(response);
+          } catch (error) {
+            console.error("Error fetching refunds:", error);
+          } finally {
+            // Ensure that the loading state is updated when refunds are fetched
+            setIsLoading(false);
+          }
+        };
+  
+        fetchRefunds();
+      }, [displayMonth, displayYear])
+    );
+
   useFocusEffect(
     useCallback(() => {
       const today = new Date();
@@ -116,6 +165,10 @@ export default function Home() {
       loadLastRefund(currentYear, currentMonth);
     }, [])
   );
+
+  useEffect(() => {
+    setSelectedTotal(process.env.EXPO_PUBLIC_TOTAL_TYPE ?? null);
+  }, []);
 
   return (
     <View
@@ -148,8 +201,237 @@ export default function Home() {
             </TouchableOpacity>
           </View>
         </View>
-        <View className="bg-white w-full h-1/4 flex rounded-lg mt-5 shadow-md p-5 border-l-4 border-l-[#FF8C00]"></View>
-        <View className="bg-white w-full h-1/6 flex rounded-lg mt-12 shadow-md p-5 border-l-4 border-l-[#FF8C00]"></View>
+        <View className="bg-white w-full h-1/4 flex rounded-lg mt-5 shadow-md border-l-4 border-l-[#FF8C00]">
+          {/* Total Refunds Section */}
+            {selectedTotal === "quantity" && (
+              <View className="flex-1 flex-col rounded-xl px-4 py-2 ml-3">
+                <View className="flex flex-col items-start">
+                  <Text className="text-xl pb-2 flex-row items-center">
+                    <Text>
+                      Reembolsos de {months[displayMonth - 1]} {displayYear}
+                    </Text>
+                  </Text>
+  
+                  {/* Total Count Section */}
+                  <View className="flex-row items-center">
+                    <Ionicons
+                      name="calculator"
+                      size={24}
+                      color="black"
+                      className="mr-2"
+                    />
+                    <Text className="font-semibold text-lg">
+                      TOTAL:{" "}
+                      <Text className="text-black font-bold">
+                        {
+                          refunds.filter((refund) =>
+                            refund.date.startsWith(
+                              `${displayYear}-${displayMonth
+                                .toString()
+                                .padStart(2, "0")}`
+                            )
+                          ).length
+                        }
+                      </Text>
+                    </Text>
+                  </View>
+  
+                  {/* Aprovados (Approved) Section */}
+                  <View className="flex-row items-center">
+                    <Ionicons
+                      name="checkmark-circle-sharp"
+                      size={26}
+                      color="green"
+                      className="mr-2"
+                    />
+                    <Text className="font-semibold text-green-500 text-lg">
+                      APROVADOS:{" "}
+                      <Text className="text-black font-bold">
+                        {
+                          refunds.filter(
+                            (refund) =>
+                              refund.date.startsWith(
+                                `${displayYear}-${displayMonth
+                                  .toString()
+                                  .padStart(2, "0")}`
+                              ) && refund.status === "approved"
+                          ).length
+                        }
+                      </Text>
+                    </Text>
+                  </View>
+  
+                  {/* Em Processamento (In-process) Section */}
+                  <View className="flex-row items-center">
+                    <Ionicons
+                      name="hourglass-sharp"
+                      size={26}
+                      color="blue"
+                      className="mr-2"
+                    />
+                    <Text className="text-blue-500 text-lg ">
+                      EM PROCESSAMENTO:{" "}
+                      <Text className="text-black font-bold">
+                        {
+                          refunds.filter(
+                            (refund) =>
+                              refund.date.startsWith(
+                                `${displayYear}-${displayMonth
+                                  .toString()
+                                  .padStart(2, "0")}`
+                              ) && refund.status === "in-process"
+                          ).length
+                        }
+                      </Text>
+                    </Text>
+                  </View>
+  
+                  {/* Rejeitados (Rejected) Section */}
+                  <View className="flex-row items-center">
+                    <Ionicons
+                      name="close-circle-sharp"
+                      size={26}
+                      color="red"
+                      className="mr-2"
+                    />
+                    <Text className="font-semibold text-red-500 text-lg">
+                      REJEITADOS:{" "}
+                      <Text className="text-black font-bold">
+                        {
+                          refunds.filter(
+                            (refund) =>
+                              refund.date.startsWith(
+                                `${displayYear}-${displayMonth
+                                  .toString()
+                                  .padStart(2, "0")}`
+                              ) && refund.status === "rejected"
+                          ).length
+                        }
+                      </Text>
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            )}
+      
+            {/* Total Value Section */}
+            {selectedTotal === "value" && (
+              <View className="flex-1 flex-col rounded-xl px-4 py-2 ml-3">
+                <View className="flex flex-col items-start">
+                  <Text className="text-xl pb-2 flex-row items-center">
+                    <Text>
+                      Reembolsos de {months[displayMonth - 1]} {displayYear}
+                    </Text>
+                  </Text>
+                  <Text>
+                    <View className="flex-row items-center">
+                      <Ionicons
+                        name="cash-outline"
+                        size={24}
+                        color="black"
+                        className="mr-2"
+                      />
+                      <Text className="font-semibold text-lg">
+                        Total a Receber :{" "}
+                        {formatCurrency(
+                          refunds
+                            .filter((refund) =>
+                              refund.date.startsWith(
+                                `${displayYear}-${displayMonth
+                                  .toString()
+                                  .padStart(2, "0")}`
+                              )
+                            )
+                            .reduce((sum, refund) => sum + refund.totalValue, 0)
+                        )}
+                      </Text>
+                    </View>
+                    {"\n"}
+                    <View className="flex-row items-center">
+                      <Ionicons
+                        name="checkmark-circle-sharp"
+                        size={24}
+                        color="green"
+                        className="mr-2"
+                      />
+                      <Text className=" text-lg text-green-500">
+                        APROVADOS:{" "}
+                        <Text className="text-black font-bold text-lg ">
+                          {formatCurrency(
+                            refunds
+                              .filter(
+                                (refund) =>
+                                  refund.date.startsWith(
+                                    `${displayYear}-${displayMonth
+                                      .toString()
+                                      .padStart(2, "0")}`
+                                  ) && refund.status === "approved"
+                              )
+                              .reduce((sum, refund) => sum + refund.totalValue, 0)
+                          )}
+                        </Text>
+                      </Text>
+                    </View>
+                    {"\n"}
+                    <View className="flex-row items-center">
+                      <Ionicons
+                        name="hourglass-sharp"
+                        size={24}
+                        color="blue"
+                        className="mr-2"
+                      />
+                      <Text className="text-blue-500 text-lg">
+                        EM PROCESSO:{" "}
+                        <Text className="text-black font-bold">
+                          {formatCurrency(
+                            refunds
+                              .filter(
+                                (refund) =>
+                                  refund.date.startsWith(
+                                    `${displayYear}-${displayMonth
+                                      .toString()
+                                      .padStart(2, "0")}`
+                                  ) && refund.status === "in-process"
+                              )
+                              .reduce((sum, refund) => sum + refund.totalValue, 0)
+                          )}
+                        </Text>
+                      </Text>
+                    </View>
+                    {"\n"}
+                    <View className="flex-row items-center">
+                      <Ionicons
+                        name="close-circle-sharp"
+                        size={24}
+                        color="red"
+                        className="mr-2"
+                      />
+                      <Text className="text-red-500 text-lg">
+                        REJEITADOS:{" "}
+                        <Text className="text-black font-bold">
+                          {formatCurrency(
+                            refunds
+                              .filter(
+                                (refund) =>
+                                  refund.date.startsWith(
+                                    `${displayYear}-${displayMonth
+                                      .toString()
+                                      .padStart(2, "0")}`
+                                  ) && refund.status === "rejected"
+                              )
+                              .reduce((sum, refund) => sum + refund.totalValue, 0)
+                          )}
+                        </Text>
+                      </Text>
+                    </View>
+                  </Text>
+                </View>
+              </View>
+            )}
+        </View>
+        <View className="bg-white w-full h-1/6 flex rounded-lg mt-12 shadow-md p-5 border-l-4 border-l-[#FF8C00]">
+            
+        </View>
       </View>
     </View>
   );
